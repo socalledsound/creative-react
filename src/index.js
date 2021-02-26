0d8932b4f4decb2dd5436279e6cac199675b0c5e
Chris Kubick <socalledsound@gmail.com>
2021-02-26 14:41:25 -0800

    react div heart end

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-const root = document.getElementById('root');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 
 const circleStyle = {
     backgroundColor: '#FF0000',
     width : '100px',
     height : '100px',
     borderRadius : '50px',
+    position : 'absolute',
 }
 
-const newDiv = document.createElement('div');
-Object.assign(newDiv.style, circleStyle);
-root.appendChild(newDiv);
+const squareStyle = {
+    backgroundColor: '#FF0000',
+    width : '100px',
+    height : '100px',
+    position : 'absolute',
+    transform: 'rotate(-45deg)',
+}
+
+const Circle = ({top, left}) => {
+    return (
+        <div style={{ ...circleStyle, top: top, left: left}}>
+
+        </div>
+    )
+}
+
+const Square = ({top, left}) => {
+    return (
+        <div style={{...squareStyle, top: top, left: left}}>
+
+        </div>
+    )
+}
+
+
+const App = () => {
+    return (
+        <div>
+            <Circle top='100px' left='90px'/>
+            <Circle top='100px' left='160px'/>
+            <Square top='135px' left='125px'/>
+        </div>
+       
+    )
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
